87678f0a15c7fb2f687bb1f408eec46be90db5bf
Valentina <valentinagaleote@gmail.com>
2022-11-21 19:59:04 +0100

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String nombre;
+        //Prepara el programa para leer datos desde el teclado
+        Scanner leer = new Scanner(System.in);
+
+        //Pide que introduzca el nombre y apellido esperando un String con nextLine()
+        System.out.print("Escribe nombre y apellido: ");
+        nombre = leer.nextLine();
+
+        System.out.println("Estudiante" + " " + nombre);
+    }
+}
